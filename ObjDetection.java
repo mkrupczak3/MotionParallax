4daa3174c92a4d2fa6a12808c9e7fe90dc0397dc
bob joe <matthew@krupczak.org>
2023-01-13 17:29:59 -0500

    add ObjDetection.java

---
+++ ObjDetection.java
@@ -0,0 +1,20 @@
+package MotionParallax;
+
+class ObjDetection {
+
+    public ObjDetection next;
+    public ObjDetection prev;
+
+    Double cartesianX;
+    Double cartesianZ;
+
+    Frame parent;
+
+    protected double reverse_bearing; // bearing from landmark to the vehicle
+
+    ObjDetection(Frame parent, double bearing, ObjDetection prev) {
+        this.parent = parent;
+        this.reverse_bearing = Utils.getReverseBearing(bearing);
+        this.prev = prev;
+    }
+}
